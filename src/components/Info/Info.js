4281bd875746e30ad638a853a28d4f855b33f6e6
alok-dotcom <64683882+alok-dotcom@users.noreply.github.com>
2023-07-01 23:19:10 +0530

    Update Info.js

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -85,7 +85,7 @@ const Info = ({ changeActive }) => {
         </h1>
         <a
           className={styles["info-section-download-btn"]}
-          href="https://drive.google.com/file/d/12KsZCy_3N5SJybM-zjAxHdjWjnFjAezV/view"
+          href="https://drive.google.com/file/d/1dXpgbCdIwVzxokYGCVA-zy5weR-yVWJ2/view?usp=sharing"
           target="_blank"
           rel="noreferrer"
           download
